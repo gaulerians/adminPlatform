558e7e26bc0acf167220f7f6d666755451c5e063
Sotelo <christian.sotelo.27@unsch.edu.pe>
2022-11-14 14:04:06 -0500

    add section uploadVideo

--- src/components/general/nabvar/listItemsNavbar.js
+++ src/components/general/nabvar/listItemsNavbar.js
@@ -1,4 +1,5 @@
 import { ReactComponent as HomeSVG } from "./../../../icons/home-1.svg";
+import { ReactComponent as UploadSVG } from "./../../../icons/simple-play.svg";
 import { ReactComponent as ExamsSVG } from "./../../../icons/exams.svg";
 import { ReactComponent as CheckSVG } from "./../../../icons/check.svg";
 import { ReactComponent as CarruselVideoSVG } from "./../../../icons/carrusel-video.svg";
@@ -8,6 +9,11 @@ export const listItemsNavbar = [
     nameItem: "Inicio",
     icon: <HomeSVG />,
     link: "/",
+  },
+  {
+    nameItem: "Subir video ",
+    icon: <UploadSVG />,
+    link: "/upload-video",
   },
   {
     nameItem: "Redacción de preguntas",

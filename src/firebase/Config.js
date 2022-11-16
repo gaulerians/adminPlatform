bfb7ff5102151f5537c56cc2077217a8409bc151
thezeivier <zbeizaga@gmail.com>
2022-11-16 06:02:46 -0500

    variables de entorno reparadas

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyDnSibx3CFf76xePNpcDanFK9NzlQBt6T4",
-    authDomain: "gauler-665ba.firebaseapp.com",
-    projectId: "gauler-665ba",
-    storageBucket: "gauler-665ba.appspot.com",
-    messagingSenderId: "501981589674",
-    appId: "1:501981589674:web:06bd9d4385281d7a3460d3",
-    measurementId: "G-07J9D8TX1E"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };

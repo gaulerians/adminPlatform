1f1e0fa24a013430eb571c01d7c0e606c345ba97
Mathayus <55334767+chsotelo@users.noreply.github.com>
2022-12-20 01:02:01 -0500

    Update

--- src/components/examns/writeQuestion/algorithms/handleChangeTextLatex.js
+++ src/components/examns/writeQuestion/algorithms/handleChangeTextLatex.js
@@ -6,40 +6,41 @@ export const handleChangeTextLatex = ({
   setSelections,
   inputRef,
 }) => {
-  if (e.type === "change") {
+  if (e.type === 'change') {
+    console.log(e.nativeEvent.inputType);
     let copiedText = Array.from(latexString);
     let lengthAdded = 0;
-    if (e.nativeEvent.inputType === "insertLineBreak") {
-      let latexFunction = " \\newline ";
+    if (e.nativeEvent.inputType === 'insertLineBreak') {
+      let latexFunction = ' \\newline ';
       copiedText.splice(selections.end, 0, latexFunction);
       lengthAdded = latexFunction.length - 1;
-    } else if (e.nativeEvent.inputType === "deleteContentBackward") {
+    } else if (e.nativeEvent.inputType === 'deleteContentBackward') {
       copiedText.splice(
-        selections.start === selections.end
-          ? selections.start - 1
-          : selections.start,
+        selections.start === selections.end ? selections.start - 1 : selections.start,
         selections.start === selections.end
           ? selections.end - selections.start + 1
           : selections.end - selections.start,
-        ""
+        '',
       );
-    } else if (e.nativeEvent.inputType === "insertText") {
-      if (e.nativeEvent.data === " ") {
-        let latexFunction = " \\space ";
+    } else if (e.nativeEvent.inputType === 'insertText') {
+      if (e.nativeEvent.data === ' ') {
+        let latexFunction = ' \\space ';
         copiedText.splice(selections.end, 0, latexFunction);
         lengthAdded = latexFunction.length - 1;
       } else {
         copiedText.splice(selections.start, 0, e.nativeEvent.data);
       }
-    } else if (e.nativeEvent.inputType === "deleteContentForward") {
-      copiedText.splice(selections.start, 1, "");
+    } else if (e.nativeEvent.inputType === 'deleteContentForward') {
+      copiedText.splice(selections.start, 1, '');
+    } else if (e.nativeEvent.inputType === 'insertFromPaste') {
+      console.log(latexString);
     }
     setSelections({
       start: inputRef.current.selectionStart + lengthAdded,
       end: inputRef.current.selectionEnd + lengthAdded,
     });
-    setLatexString(copiedText.join(""));
-  } else if (e.type === "select") {
+    setLatexString(copiedText.join(''));
+  } else if (e.type === 'select') {
     setSelections({
       start: inputRef.current.selectionStart,
       end: inputRef.current.selectionEnd,
